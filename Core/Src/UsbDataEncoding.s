ef08c29329f33e03c70f550733c5576e533814c9
Renan <renamp2006@gmail.com>
2026-06-10 18:08:15 +0200

    [Add decoding function Rawdiff2Bytes]
     - Add assembly routine, Rawdiff2Bytes, to decode raw packed
     differential USB bits back to data bytes including handling
     of bit-stuffing.
    
     - Update Bytes2Rawdiff save/restore to backup LR register

--- Core/Src/UsbDataEncoding.s
+++ Core/Src/UsbDataEncoding.s
@@ -21,6 +21,9 @@
 .global Bytes2Rawdiff
 .type Bytes2Rawdiff, %function
 
+.global Rawdiff2Bytes
+.type Rawdiff2Bytes, %function
+
 /**
  * @brief  Converts an array of data bytes to raw differential format for 
  *         USB transmission.
@@ -34,11 +37,12 @@
  *     
  */
 Bytes2Rawdiff:
-    push    {r4-r7}
+    push    {r4-r7, lr}
     mov     r7, r8
     mov     r6, r9
     mov     r5, r10
-    push    {r5-r7}
+    mov     r4, r11
+    push    {r4-r7}
 
     mov     r7, sp
     subs    r7, #4          @ (x) rawdiff output pointer (top of stack)
@@ -100,9 +104,103 @@ Bytes2Rawdiff_L4:
     mov     r0, r9          @ len diff bits
     mov     r1, sp         
     subs    r1, #4          @ ptr array diff
-    pop     {r5-r7}         @ restore r10,r9,r8 into r5,r6,r7
+    pop     {r4-r7}         @ restore r10,r9,r8 into r5,r6,r7
+    mov     r11, r4
     mov     r10, r5
     mov     r9, r6
     mov     r8, r7
-    pop     {r4-r7}         @ restore r4-r7
-    bx      lr
+    pop     {r4-r7, pc}         @ restore r4-r7
+
+
+.equ NUMBER_DIFF_BITS_IN_BYTE,    (0x04)
+.equ BITSTUFF_CNT_RST,            (0x06)
+.equ INITIAL_DIFF,                (0xAA)
+.equ DIFF_MASK,                   (0x03)
+.equ SET_BIT,                     (0x80)
+
+/**
+ * @brief  Converts an array of raw differential bits to data bytes.
+ * @param  r0: Length of the input raw differential data in bits
+ * @param  r1: Pointer to the raw differential data array(stored on the stack)
+ * @param  r2: Pointer to the data array to store output bytes
+ * @retval r0: Length of the output data in bytes
+ * @note
+ *
+ */
+Rawdiff2Bytes:
+    push  {r4-r7, lr}
+    mov   r4, r8
+    mov   r5, r9
+    mov   r6, r10
+    mov   r7, r11
+    push  {r4-r7}
+    movs  r7, #1          @
+    mov   r12, r7         @ [r12] always 1 (for inc operations)
+    movs  r7, #0
+    mov   r8, r7          @ clear data bytes counter
+    mov   r11, r7         @ clear (T)[r11] skip bitstuff
+    mov   r6, r7          @ clear Decoding data byte
+    movs  r7, 0x08
+    mov   r9, r7          @ [r9] count data_bits
+    ldr   r7, =NUMBER_DIFF_BITS_IN_BYTE
+    mov   r10, r7         @ [r10] load number of differential bits
+    ldr   r3, =BITSTUFF_CNT_RST  @ [r3] reset bitstuff counter
+    ldr   r4, =INITIAL_DIFF      @ [r4] store Initial diff state
+Rawdiff2BytesI0:
+    ldrb  r5, [r1]        @ load to [r5] next packed bitstuff from array
+    subs  r1, #1          @ update point to next element
+Rawdiff2BytesI1:
+    ldr   r7, =DIFF_MASK  @ Mask for next differential (D- and D+) respective
+    ands  r7, r5          @ move masked bits to decode
+    eors  r4, r7          @ check state of bits. if didnt change result==0
+    mov   r4, r7          @ store actual state for next check
+    bne  Rawdiff2BytesL1  @ branch if state changed
+    lsrs  r6, #1
+    subs  r3, #1          @ decrement bitstuff counter [r3]
+    bne  Rawdiff2BytesL2  @ branch if not bitstuff
+    mov   r11, r12        @ set (T)[r11] skip next bit. it is a bitstuffed
+    ldr   r3, =BITSTUFF_CNT_RST
+Rawdiff2BytesL2:
+    ldr   r7, =SET_BIT    @ load set bit
+    orrs  r6, r7          @ update current decoding data byte
+    b    Rawdiff2BytesL3
+Rawdiff2BytesS1:
+    movs  r7, #0
+    mov   r11, r7         @ clear (T)[r11] skip bitstuff
+    b    Rawdiff2BytesL4
+Rawdiff2BytesL1:
+    mov   r7, r11
+    ands  r7, r7
+    bne  Rawdiff2BytesS1  @ it is bitstuff - goto skip bitstuff
+    lsrs  r6, #1          @ update current decoding data byte
+    ldr   r3, =BITSTUFF_CNT_RST
+Rawdiff2BytesL3:          @ check number of bits
+    mov   r7, r9          @ load data_bits counter
+    subs  r7, #1          @ decrement
+    mov   r9, r7          @ store data_bits counter back to [r9]
+    bne  Rawdiff2BytesL4  @ branch when counter==0
+    movs  r7, #0x08       @ counter data_bits reset value
+    mov   r9, r7          @ store data_bits counter [r9]
+    strb  r6, [r2]        @ store decoded data
+    adds  r2, #1          @ Increment data pointer
+    add   r8, r12         @ incremment decoded data counter [r8]
+    movs  r6, #0          @ clear Decoding data byte
+Rawdiff2BytesL4:          @ check diff len
+    subs  r0, #1          @ decrement len diff bits
+    beq  Rawdiff2BytesEND
+    lsrs  r5, #2          @ rotate to next differential (D- and D+)
+    mov   r7, r10         @ load counter differential bits in byte
+    subs  r7, #1          @ decrement
+    mov   r10, r7         @ store the counter back
+    bne  Rawdiff2BytesI1
+    ldr   r7, =NUMBER_DIFF_BITS_IN_BYTE            @
+    mov   r10, r7
+    b    Rawdiff2BytesI0
+Rawdiff2BytesEND:
+    mov   r0, r8          @ return len of decoded data bytes
+    pop   {r4-r7}         @ restore r10,r9,r8 into r4,r5,r6
+    mov   r8, r4
+    mov   r9, r5
+    mov   r10, r6
+    mov   r11, r7
+    pop   {r4-r7, pc}     @ restore r4-r7 and return
